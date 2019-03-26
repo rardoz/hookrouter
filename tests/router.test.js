99a683b76fc9e3d0e5ad3ba58b199c48693f61ba
Robert Greene <robert.greene@bcgdv.com>
2019-03-26 10:52:31 -0700

    Stub tests for router

---
+++ tests/router.test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {navigate, setPath, getPath, useRoutes} from '../src/router'
+
+describe("Router", () => {
+  it('should navigate', () => {
+
+  })
+
+  it('should set path', () => {
+
+  })
+
+  it('should get path', () => {
+
+  })
+  
+  it('should use routes', () => {
+    
+  })
+})
